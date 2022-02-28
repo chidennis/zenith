16757da890a6ee3bd4db38fe670e492473462414
brian.rexrode <brian.rexrode@defenseunicorns.com>
2022-02-28 12:19:47 -0700

    add git var

--- base/beast-core/files/data-loader.sh
+++ base/beast-core/files/data-loader.sh
@@ -29,6 +29,7 @@ then
     find "$VOLUME_PATH" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -r0 rm -R
     rm -rf $VOLUME_PATH
     echo "Pulling data from '$GIT_REPO/' to '$VOLUME_PATH'.."
+    export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
     GIT_LFS_SKIP_SMUDGE=1 git clone "https://$USERNAME:$TOKEN@$GIT_REPO" "$VOLUME_PATH"
 fi
 
